e146e0feb077e44cdfacf8bd4c1b13049673be84
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-07-25 13:20:33 +0800

    update GETREADME.py

--- GETREADME.py
+++ GETREADME.py
@@ -1,4 +1,17 @@
 import requests
 
 
-r = requests.get("https://github.com/pynickle/python-cheatsheet/")
+def main():
+    url = "https://raw.githubusercontent.com/pynickle/python-cheatsheet/master/README.md"
+    headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3858.0 Safari/537.36"}
+
+    r = requests.get(url, headers=headers)
+    f = open("python-cheatsheet.md", "w", encoding="utf-8")
+
+    res = r.text.replace("\r\n", "\n")
+    print(res, file = f)
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
