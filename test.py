17fb1b9a3a15d0d29cea12eed6449a8e9c500e86
pynickle <2330458484@qq.com>
2019-07-29 04:44:11 +0000

    improve test

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import re
 
 
 NOTEST = ["random.random", "random.randint", "random.randrange",
-          "random.uniform", "codecs.getdecoder", "codecs.getencoder"]
+          "random.uniform", "codecs.getdecoder", "codecs.getencoder",
+          "time.ctime", "time.perf_counter", "time.strftime",
+          "time.localtime", "os.getcwd", "os.name", "secrets.choice"]
 
 def main(file_name):
     status = None
