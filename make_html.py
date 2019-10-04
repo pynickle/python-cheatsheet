edb2d5de6706a790c279106b66b6735694be9c8f
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-10-04 21:06:06 +0800

    add web

---
+++ make_html.py
@@ -0,0 +1,35 @@
+import os
+
+from GETREADME import main
+
+main("2", "1")
+
+os.popen("pandoc python-cheatsheet.md -t html -o python-cheatsheet.html")
+
+with open("python-cheatsheet.html", "r", encoding="utf-8") as f:
+    data = f.read()
+os.remove("python-cheatsheet.html")
+
+before = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>python-cheatsheet by pynickle</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='index.css') }}">
+    <link rel="stylesheet" href="{{ url_for('static', filename='github.css') }}">
+    <script src="{{ url_for('static', filename='highlight.pack.js') }}"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+"""
+
+after = """
+</body>
+</html>
+"""
+
+data = before + data + after
+
+with open("./web/templates/python-cheatsheet.html", "w", encoding="utf-8") as f:
+    f.write(data)
--- web/app.py
+++ web/app.py
@@ -5,7 +5,7 @@ app.config.from_pyfile("config.py")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("python-cheatsheet.html")
 
 if __name__ == "__main__":
     app.run(port=5555)
