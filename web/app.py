3aa03b4eb93353754105c12e15284619bb96fe8c
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-10-06 16:34:58 +0800

    fix

--- web/app.py
+++ web/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__, instance_relative_config=True)
@@ -6,6 +8,11 @@ app.config.from_pyfile("config.py")
 @app.route("/")
 def index():
     return render_template("python-cheatsheet.html")
+
+@app.route("/post", methods=["POST"])
+def post(data):
+    data = json.loads(data)
+    print(data)
 
 if __name__ == "__main__":
     app.run(port=5555)
